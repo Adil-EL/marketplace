acd9c72359aabf5de24440ae1b6c1c756d050aa3
Adil-EL <adilelhakouni.lt@gmail.com>
2021-11-10 23:53:14 +0100

    good to go

--- run.py
+++ run.py
@@ -7,11 +7,13 @@ import configparser
 config = configparser.ConfigParser()
 config.read(os.path.abspath(os.path.join(".ini")))
 
+
+app = create_app()
+app.config['DEBUG'] = True
+app.config['LAPTOPS_DB_URI'] = config['PROD']['LAPTOPS_DB_URI']
+app.config['LAPTOPS_DB_NAME'] = config['PROD']['LAPTOPS_DB_NAME']
+app.config['SECRET_KEY'] = config['PROD']['SECRET_KEY']
+
 if __name__ == "__main__":
-    app = create_app()
-    app.config['DEBUG'] = True
-    app.config['LAPTOPS_DB_URI'] = config['PROD']['LAPTOPS_DB_URI']
-    app.config['LAPTOPS_DB_NAME'] = config['PROD']['LAPTOPS_DB_NAME']
-    app.config['SECRET_KEY'] = config['PROD']['SECRET_KEY']
 
     app.run()
